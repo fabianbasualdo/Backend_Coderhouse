1e9b9d379e159ecd3ef7203187dfd44ca8def9db
Marcelo Fabian Basualdo <fabianbausaldo0909@gmail.com>
2022-10-22 21:55:27 -0300

    clases

--- DESAFIO1/app.js
+++ DESAFIO1/app.js
@@ -1,48 +1,69 @@
 class Usuario {
-
-    constructor(nombre, apellido) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.libro = [
-            { nombre: "Los secretos de la mente millonaria", autor: "T. Harv Eker" },
-            { nombre: "Piense y hagase rico", autor: "Napoleon Hill" },
-            { nombre: "El poder del ahora", autor: "Eckhart Tolle" }
-        ];
-
-        this.mascotas = ["Perro", "Gato", "Tortuga"];
+    constructor(nuevoNombre, nuevoApellido, nuevoLibros, nuevoMascotas) {
+        this.nombre = nuevoNombre
+        this.apellido = nuevoApellido
+        this.libros = nuevoLibros
+        this.mascotas = nuevoMascotas
     }
-
     getFullName() {
-        console.log(`Su nombre es: ${this.nombre} y su apellido es ${this.apellido}`)
+        return console.log(`Su nombre es ${this.nombre} y su apellido es ${this.apellido}`)
     }
-
     addMascota(nuevaMascota) {
-        this.mascotas.push(nuevaMascota);
-        console.log(`La mascota fue agrega, el usuario ahora tiene : ${this.mascotas}`);
+        this.mascotas.push(nuevaMascota)
+    
+    }
+    countMascotas() {
+        return console.log(`La cantidad de mascotas son: ${this.mascotas.length}`)
+    }
+    addBook(nuevoNombre, nuevoAutor) {
+        this.libros.push({ nombre: nuevoNombre, autor: nuevoAutor })
     }
 
-    countMacotas() {
-        console.log(`La cantidad de mascotas que tiene el usuario es: ${this.mascotas.length}`)
+
+    getBookNames() {
+        const nombreLibros = []
+        this.libros.forEach(libro => {
+            nombreLibros.push(libro.nombre)
+        })
+        console.log("Los libros de este cliente son:")
+        return nombreLibros.forEach(nombrelib => console.log(`${nombrelib}, \n`))
+
     }
 
-    addBooks(nuevoLibro, nuevoAutor) {
-        this.libro.push({nombre: nuevoLibro, autor: nuevoAutor})
-        console.log(`El libro fue agregado ${nuevoLibro}`)
+
+    getMascotas() {
+        console.log("Las mascotas de este cliente son:");
+        return this.mascotas.forEach(mascota => { console.log(` ${mascota},\n`) })
     }
-
-    getBookNames(){
-
-        let NombreLibro = []
-        this.libro.forEach(libros => NombreLibro.push(libros.nombre))
-        
-        console.log(NombreLibro)
-    }
-
 }
 
-const cliente = new Usuario("Marcelo Fabian", "Basualdo");
-cliente.getFullName();
-cliente.addMascota("Loro");
-cliente.countMacotas();
-cliente.addBooks("Padre rico, padre pobre", "Robert Kiyosaki");
-cliente.getBookNames();
+//creo el objeto llamado cliente
+//coloco los valores al constructor, con new reservo espacio en memoria para dicho objeto
+const cliente = new Usuario("Marcelo", "Basualdo", [{ nombre: "Piense y hagase rico", autor: "Napoleon Hill" }], ["Gato"])
+
+//llamo a los metodos de la clase, que haran referencia al objeto cliente
+cliente.getFullName()
+cliente.addMascota("Perro")
+cliente.countMascotas()
+cliente.addBook("El poder del ahora", "Eckhart Tolle")
+cliente.addBook("Los secretos de la mente millonaria", "T. Harv Eker")
+
+//los metodos get los uso para mostrar en pantalla el valor de los atributos
+cliente.getBookNames()
+cliente.getMascotas()
+
+
+//creo el objeto llamado cliente2
+//coloco los valores al constructor, con new reservo espacio en memoria para dicho objeto
+const cliente2=new Usuario("Roberto","Ledesma",[{nombre:"El hombre mas rico de babilonia",autor:"George Clason"}],["Tortuga"])
+
+//llamo a los metodos de la clase, que haran referencia al objeto cliente2
+cliente2.getFullName()
+cliente2.addMascota("Mono")
+cliente2.addMascota("Paloma")
+cliente2.countMascotas()
+cliente2.addBook("Tus zonas erroneas", "Wayne Dyer")
+
+//los metodos get los uso para mostrar en pantalla el valor de los atributos
+cliente2.getBookNames()
+cliente2.getMascotas()
