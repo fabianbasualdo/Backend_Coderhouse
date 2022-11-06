38b882144942bebf997911fb1a747499d0d570d9
Marcelo Fabian Basualdo <fabianbausaldo0909@gmail.com>
2022-11-06 20:30:41 -0300

    Express

---
+++ DESAFIO3_ServidorConExpress/index.js
@@ -0,0 +1,26 @@
+const {Contenedor} = require('./productos.js')
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 8080
+const productos = new Contenedor("productos.txt")
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
+app.on("error", error => console.log(`Error en servidor ${error}`))
+
+
+app.get('/',(req, res)=>{
+    res.send('<h1 style = "color: red">Bienvenidos</h1>')
+})
+app.get('/productos', async (req, res)=>{
+    const totalProductos = await productos.getAll()
+    res.send({totalProductos})
+})
+app.get('/productosRandom', async (req, res)=>{
+    const id = Math.floor(Math.random() * 3);
+    const item = await productos.getById(id)
+    res.send({item})
+}) 
