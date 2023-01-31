1479532e30d835043fa7c0bebae4ac909bab165f
Marcelo Fabian Basualdo <fabianbausaldo0909@gmail.com>
2023-01-31 12:44:16 -0300

    desafio11 inicioDeSesion

--- DESAFIO9_10_Normalizacion_Sesiones/utils/normalizr.js
+++ DESAFIO9_10_Normalizacion_Sesiones/utils/normalizr.js
@@ -1,10 +1,15 @@
 const { normalize, denormalize, schema } = require('normalizr')
 
+
 const normalizeAndDenormalize = (what, obj) => {
+
     const authorSchema = new schema.Entity("author")
+
+
     const chatSchema = new schema.Entity("messages", {
         author: authorSchema,
     })
+
 
     if(what == "normalize") {
         return normalize(obj, [chatSchema])
